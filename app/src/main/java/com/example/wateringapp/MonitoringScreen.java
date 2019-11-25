91d27a9c652c5e9d1a6e9dc223a297df0394b37e
juanpablomillan <pablomillansotelo@gmail.com>
2019-11-25 01:42:13 -0600

    Initial commit

---
+++ app/src/main/java/com/example/wateringapp/MonitoringScreen.java
@@ -0,0 +1,228 @@
+package com.example.wateringapp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MonitoringScreen extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private int mMaxChars = 50000;
+    private UUID mDeviceUUID;
+    private BluetoothSocket mBTSocket;
+    private ReadInput mReadThread = null;
+
+
+    private boolean mIsUserInitiatedDisconnect = false;
+
+    private TextView mTxtReceiveOne;
+    private CheckBox chkReceiveText;
+
+
+    private boolean mIsBluetoothConnected = false;
+
+    private BluetoothDevice mDevice;
+
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitoring_screen);
+        ActivityHelper.initialize(this);
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+        mDevice = b.getParcelable(MainActivity.DEVICE_EXTRA);
+        mDeviceUUID = UUID.fromString(b.getString(MainActivity.DEVICE_UUID));
+        mMaxChars = b.getInt(MainActivity.BUFFER_SIZE);
+        Log.d(TAG, "Ready");
+        mTxtReceiveOne = (TextView) findViewById(R.id.txtReceiveOne);
+        chkReceiveText = (CheckBox) findViewById(R.id.chkReceiveText);
+
+
+    }
+
+    private class ReadInput implements Runnable {
+
+        private boolean bStop = false;
+        private Thread t;
+
+        public ReadInput() {
+            t = new Thread(this, "Input Thread");
+            t.start();
+        }
+
+        public boolean isRunning() {
+            return t.isAlive();
+        }
+
+        @Override
+        public void run() {
+            InputStream inputStream;
+
+            try {
+                inputStream = mBTSocket.getInputStream();
+                while (!bStop) {
+                    byte[] buffer = new byte[256];
+                    if (inputStream.available() > 0) {
+                        inputStream.read(buffer);
+                        int i = 0;
+
+                        for (i = 0; i < buffer.length && buffer[i] != 0; i++) {
+                        }
+                        final String strInput = new String(buffer, 0, i);
+
+                        if (chkReceiveText.isChecked()) {
+                            mTxtReceiveOne.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mTxtReceiveOne.setText(strInput+" %");
+
+                                }
+                            });
+                        }
+                    }
+                    Thread.sleep(1000);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void stop() {
+            bStop = true;
+        }
+
+    }
+
+    private class DisConnectBT extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            if (mReadThread != null) {
+                mReadThread.stop();
+                while (mReadThread.isRunning())
+                    ; // Wait until it stops
+                mReadThread = null;
+
+            }
+
+            try {
+                mBTSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            mIsBluetoothConnected = false;
+            if (mIsUserInitiatedDisconnect) {
+                finish();
+            }
+        }
+
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mBTSocket != null && mIsBluetoothConnected) {
+            new DisConnectBT().execute();
+        }
+        Log.d(TAG, "Paused");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mBTSocket == null || !mIsBluetoothConnected) {
+            new ConnectBT().execute();
+        }
+        Log.d(TAG, "Resumed");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopped");
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean mConnectSuccessful = true;
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(MonitoringScreen.this, "Espere", "Conectando...");
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+
+            try {
+                if (mBTSocket == null || !mIsBluetoothConnected) {
+                    mBTSocket = mDevice.createInsecureRfcommSocketToServiceRecord(mDeviceUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    mBTSocket.connect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mConnectSuccessful = false;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            if (!mConnectSuccessful) {
+                Toast.makeText(getApplicationContext(), "No se pudo conectar al dispositivo. Compruebe que es el dispositivo BT HC05 y que la configuracion UUID es correcta", Toast.LENGTH_LONG).show();
+                finish();
+            } else {
+                msg("Conectado al dispositivo");
+                mIsBluetoothConnected = true;
+                mReadThread = new ReadInput();
+            }
+
+            progressDialog.dismiss();
+        }
+
+    }
+
+
+
+}
